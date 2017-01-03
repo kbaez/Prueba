0c65ec105fb9ea7df30453b88b3f6511f3ccd988
Karina Baez <karina.baez@synapse.com>
2017-01-03 09:26:45 -0300

    Initial commit

---
+++ app/src/main/java/com/example/karinabaez/prueba/Lienzo.java
@@ -0,0 +1,15 @@
+package com.example.karinabaez.prueba;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by karina.baez on 03/01/2017.
+ */
+
+public class Lienzo extends View {
+    public Lienzo(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
